13a2641d2dcb0eb07f627912dd1dc9b0896b26c0
Joe Maples <joe@frap129.org>
2018-01-15 18:53:35 -0500

    Add prebuilts for Now Playing
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ AmbientMusic.mk
@@ -0,0 +1,12 @@
+# AmbientSense files
+PRODUCT_COPY_FILES += \
+    vendor/ambientmusic/etc/ambient/matcher_tah.leveldb:system/etc/ambient/matcher_tah.leveldb \
+    vendor/ambientmusic/etc/firmware/dnd.descriptor:system/etc/firmware/dnd.descriptor \
+    vendor/ambientmusic/etc/firmware/dnd.sound_model:system/etc/firmware/dnd.sound_model \
+    vendor/ambientmusic/etc/firmware/music_detector.descriptor:system/etc/firmware/music_detector.descriptor \
+    vendor/ambientmusic/etc/firmware/music_detector.sound_model:system/etc/firmware/music_detector.sound_model \
+    vendor/ambientmusic/etc/sysconfig/ambientsense.xml:system/etc/sysconfig/ambientsense.xml
+
+# AmbientSense app
+PRODUCT_PACKAGES += \
+    AmbientSensePrebuilt
