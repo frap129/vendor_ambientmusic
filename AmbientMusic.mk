167c0c08140a454e8c3f720e28b3ffde55f4912d
gtpitch <bmontagu@gmail.com>
2018-01-15 22:00:48 -0500

    fix name of permissions file
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- AmbientMusic.mk
+++ AmbientMusic.mk
@@ -5,7 +5,7 @@ PRODUCT_COPY_FILES += \
     vendor/ambientmusic/etc/firmware/dnd.sound_model:system/etc/firmware/dnd.sound_model \
     vendor/ambientmusic/etc/firmware/music_detector.descriptor:system/etc/firmware/music_detector.descriptor \
     vendor/ambientmusic/etc/firmware/music_detector.sound_model:system/etc/firmware/music_detector.sound_model \
-    vendor/ambientmusic/etc/permissions/ambientsense.xml:system/etc/permissions/ambientsense.xml \
+    vendor/ambientmusic/etc/permissions/AmbientSensePrebuilt.xml:system/etc/permissions/AmbientSensePrebuilt.xml \
     vendor/ambientmusic/etc/sysconfig/ambientsense.xml:system/etc/sysconfig/ambientsense.xml
 
 # AmbientSense app
